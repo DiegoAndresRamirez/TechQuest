e98d8a6b4ffac43e9629f77e814acc34e3797a0d
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-27 01:54:19 -0500

    create all services

--- src/main/java/com/riwi/TechQuest/infrastructure/services/MissionService.java
+++ src/main/java/com/riwi/TechQuest/infrastructure/services/MissionService.java
@@ -10,5 +10,4 @@ public class MissionService {
     @Autowired
     private MissionRepository missionRepository;
 
-
 }
--- src/main/java/com/riwi/TechQuest/infrastructure/services/SkillService.java
+++ src/main/java/com/riwi/TechQuest/infrastructure/services/SkillService.java
@@ -1,7 +1,12 @@
 package com.riwi.TechQuest.infrastructure.services;
 
+import com.riwi.TechQuest.domain.persistence.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SkillService {
+
+    @Autowired
+    private SkillRepository skillRepository;
 }
